4895734b65e83c57494a3d6e52def8c841f50fff
hamzariazm <hamza.riaz@empglabs.com>
2023-05-18 15:08:39 +0500

    redux-implementation

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getPosts } from './app/features/posts/postsSlice';
+
+function App() {
+  const {entities, loading} = useSelector((state)=>state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getPosts());
+  }, [])
+
+  if (loading) return <p>Loading...</p>
+
+  return(entities)?(
+    <div>
+      <h2>Blog Posts</h2>
+      {entities.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  ): <></>
+}
+
+export default App;
